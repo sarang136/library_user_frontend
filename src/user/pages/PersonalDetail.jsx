d930e7c811bb6ff7019782ec4cee656555892fcf
gopi-gh <gopi-gh@d3vtech.com>
2025-10-18 12:36:46 +0530

    cookied

--- src/user/pages/PersonalDetail.jsx
+++ src/user/pages/PersonalDetail.jsx
@@ -188,7 +188,8 @@ const PersonalDetail = () => {
         </div>
     );
 };
-
+// 
+// 
 const DetailInput = ({ label, value }) => (
     <div className="flex flex-col">
         <label className="text-sm font-medium text-gray-700 mb-1">{label}</label>
